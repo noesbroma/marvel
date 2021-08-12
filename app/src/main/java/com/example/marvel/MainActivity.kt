2d5d1ffe0ee95b0805cf6dd9de1714b3ae16eb81
Judit <noesbroma@hotmail.com>
2021-08-12 09:57:19 +0200

    Add HomeFragment

--- app/src/main/java/com/example/marvel/MainActivity.kt
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.marvel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.marvel.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fragment = HomeFragment.newInstance()
+        openFragment(fragment)
+    }
 
-        var a = "dd"
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/example/marvel/home/HomeFragment.kt
+++ app/src/main/java/com/example/marvel/home/HomeFragment.kt
@@ -1,4 +1,33 @@
 package com.example.marvel.home
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.marvel.R
+
+class HomeFragment: Fragment() {
+    companion object {
+        fun newInstance(): HomeFragment {
+            val fragment = HomeFragment()
+
+            return fragment
+        }
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /*setUI()
+        setListeners()
+        observeViewModel()*/
+    }
+
 }
