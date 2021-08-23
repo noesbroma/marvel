38ee7fe55e89027548e7959f414000756338cdb7
Judit <noesbroma@hotmail.com>
2021-08-23 19:39:17 +0200

    Improve

--- app/src/main/java/com/example/marvel/ui/MainActivity.kt
+++ app/src/main/java/com/example/marvel/ui/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.example.marvel.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.example.marvel.MarvelApplication
 import com.example.marvel.R
-import com.example.marvel.ui.detail.DetailFragment
 import com.example.marvel.ui.list.ListFragment
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+
         val fragment = ListFragment.newInstance()
         openFragment(fragment)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
 
--- app/src/main/java/com/example/marvel/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/marvel/ui/list/ListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.marvel.R
@@ -11,10 +12,11 @@ import com.example.marvel.data.list.Character
 import com.example.marvel.data.list.CharactersRecyclerAdapter
 import com.example.marvel.ui.MainActivity
 import com.example.marvel.ui.detail.DetailFragment
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import kotlinx.android.synthetic.main.fragment_list.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ListFragment: Fragment() {
+
     companion object {
         fun newInstance(): ListFragment {
             val fragment = ListFragment()
@@ -29,7 +31,12 @@ class ListFragment: Fragment() {
     private var charactersList: ArrayList<Character> = ArrayList<Character>()
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
@@ -44,28 +51,34 @@ class ListFragment: Fragment() {
 
 
     private fun observeViewModel() {
-        viewModel.onLoadCharactersEvent.observe(viewLifecycleOwner, androidx.lifecycle.Observer { characters ->
-            linearLayoutManager = LinearLayoutManager(context)
-            charactersRecycler.layoutManager = linearLayoutManager
-            charactersRecycler.hasFixedSize()
+        viewModel.onLoadCharactersEvent.observe(
+            viewLifecycleOwner,
+            androidx.lifecycle.Observer { characters ->
+                linearLayoutManager = LinearLayoutManager(context)
+                charactersRecycler.layoutManager = linearLayoutManager
+                charactersRecycler.hasFixedSize()
 
-            charactersList = characters
+                charactersList = characters
 
-            val mAdapter = context?.let { CharactersRecyclerAdapter(charactersList) }
+                val mAdapter = context?.let { CharactersRecyclerAdapter(charactersList) }
 
-            mAdapter?.setOnItemClickListener(object : CharactersRecyclerAdapter.ClickListener {
-                override fun onItemClick(v: View, position: Int) {
-                    (activity as MainActivity).openFragment(DetailFragment.newInstance(characters[position]))
+                mAdapter?.setOnItemClickListener(object : CharactersRecyclerAdapter.ClickListener {
+                    override fun onItemClick(v: View, position: Int) {
+                        (activity as MainActivity).openFragment(
+                            DetailFragment.newInstance(
+                                characters[position]
+                            )
+                        )
+                    }
+
+                })
+
+                if (charactersList.size > 0) {
+                    charactersRecycler.adapter?.notifyDataSetChanged()
+                    charactersRecycler.adapter = mAdapter
+                } else {
+                    //noResults.visibility = View.VISIBLE
                 }
-
             })
-
-            if (charactersList.size > 0) {
-                charactersRecycler.adapter?.notifyDataSetChanged()
-                charactersRecycler.adapter = mAdapter
-            } else {
-                //noResults.visibility = View.VISIBLE
-            }
-        })
     }
 }
